489b90028671bf3975da580bc082b24c45e1c0c7
Stu Thompson <stuart@stuartthompson.net>
2021-05-17 13:55:52 -0700

    Files: Improves JSDoc on file management functions.

--- src/files.js
+++ src/files.js
@@ -2,14 +2,22 @@ import fs from 'fs';
 import mkdirp from 'mkdirp';
 import path from 'path';
 
-/// Reads a file from disk
+/**
+ * Reads a file from disk.
+ * @param {*} filename - The name of the file to read.
+ * @returns The requested file if found, null otherwise.
+ */
 const readFile = (filename) => {
     const contents =
         fs.readFileSync(path.join(path.resolve(), filename), 'utf8');
     return contents;
 }
 
-/// Saves a file to disk
+/**
+ * Saves a file to disk.
+ * @param {*} filename - The name of the file to save.
+ * @param {*} contents - The file contents.
+ */
 const saveFile = (filename, contents) => {
     const dir = path.dirname(filename);
     mkdirp.sync(dir);
